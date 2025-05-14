70aeac69b62d1b5a131da8e2c5b0f36f2044c0d4
Zarostha <Github4096@gmail.com>
2025-05-14 10:05:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Result of the form
+function result (form) {
+    alert (
+        "These are your details,"
+        + "\nName : " + form.name.value
+        + "\nPassword : " + form.password.value
+        + "\nRecovery Email : " + form.email.value
+    );
+
+    loacation.reaload();
+};
